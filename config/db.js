015e5ee276bf9bd93e67f7053a117e1d0c22ec22
Gauravbio <93136782+Gauravbio@users.noreply.github.com>
2023-01-18 22:43:27 +0530

    vercel

--- config/db.js
+++ config/db.js
@@ -2,4 +2,4 @@ const mongoose=require("mongoose");
 
 mongoose.set({strictQuery:false});
 
-exports.connectDatabase=()=> {mongoose.connect("mongodb+srv://Gaurav:g6p25p2v11@cluster0.cte7qkn.mongodb.net/Minor?retryWrites=true&w=majority").then((con)=> console.log("database connected")).catch((err)=>console.log(err));}
+exports.connectDatabase=()=> {mongoose.connect(process.env.MONGO_URI).then((con)=> console.log("database connected")).catch((err)=>console.log(err));}
